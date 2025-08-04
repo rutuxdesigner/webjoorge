19cc7cfb52ce8b18de0619bab10c419b679e4906
rutuxdesigner <rutuxdesigner@gmail.com>
2025-08-04 17:16:08 +0200

    Commin .JS practica

--- practica.js
+++ practica.js
@@ -8,3 +8,57 @@ x=3*2
 6
 x=6/2
 3
+
+/*CONJUNTO DE DATOS*/
+
+edades = [22,28,34,35,43,52]
+edades[2]
+
+macedonia= ["pera", "piña", "Sandia", "cereza"]
+macedonia [3]/*posición 3*/
+
+
+/*OBJETOS*/
+
+coche={
+    marca:"seat",
+    modelo:"Ibiza",
+    color:"blanco",
+    Kms: 47000,
+    usado: true,
+}
+
+coche.modelo
+"Ibiza"
+coche.color
+"blanco"
+coche.usado
+"true"
+
+/* ARRAY DE OBJETOS*/
+
+coches = [
+    {
+        marca:"audi",
+        modelo: "A3",
+        color: "negro",
+        Kms:17000,
+        usado: true,
+    },
+
+    {
+        marca:"BMW",
+        modelo:"318",
+        color: "Blanco",
+        Kms: 0,
+        usado: false
+    }
+]
+
+coches[0] Audi, A3, negro, 17000, true
+coches [1] BMW, 318, BLANCO, 0, false
+
+
+
+
+
