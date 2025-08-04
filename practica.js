cd4bb8bea0cb3bd68b6b473fea5d7a34bb27d507
rutuxdesigner <rutuxdesigner@gmail.com>
2025-08-04 19:04:15 +0200

    2 commit practica

--- practica.js
+++ practica.js
@@ -1,14 +1,3 @@
-/* OPERAR EN JS*/
-
-x = 3+2
-5
-x=3-2
-1
-x=3*2
-6
-x=6/2
-3
-
 /*CONJUNTO DE DATOS*/
 
 edades = [22,28,34,35,43,52]
@@ -57,6 +46,73 @@ coches = [
 
 coches[0] Audi, A3, negro, 17000, true
 coches [1] BMW, 318, BLANCO, 0, false
+
+
+/* OPERAR EN JS*/
+
+x = 3+2
+5
+x=3-2
+1
+x=3*2
+6
+x=6/2
+3
+
+likes = 300
+300
+followers = 600
+600
+/*CUANTOS SEGUIDORES HACEN LIKE*/
+engangement = likes/followers
+0.5 /*la mitad de los followers han hecho like*/
+
+likes = 300
+300
+followers = 600
+600
+engagement = likes/followers
+0.5
+pregunta un numero es par?
+VM339:1 Uncaught SyntaxError: Unexpected identifier 'un'Understand this error
+resto = 4%2
+0
+resto= 4%3
+1
+resto es = lo que sobra de la división cuando no es perfecta que no son par (se suele usar para paginar. Imprimes 1000 paginas= )
+VM754:1 Uncaught SyntaxError: Unexpected identifier 'es'Understand this error
+likes = 30 
+30
+likes= likes +1
+31
+likes = likes ++
+31
+likes
+31
+likes ++
+31
+likes ++
+32
+likes ++
+33
+likes --
+34
+likes --
+33
+likes
+32
+media = 7.5 
+7.5
+media ++
+7.5
+media
+8.5
+media --
+8.5
+media 
+7.5
+
+
 
 
 
